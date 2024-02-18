ccbc1c12f5327a0fb1163d5ac29a96de1642d1a1
TheLastHobbit <2865755738@qq.com>
2024-02-18 12:03:24 +0800

    frontend util add

--- web/src/utils/Market.js
+++ web/src/utils/Market.js
@@ -11,16 +11,32 @@ export async function buy(seller, shopAddr, _id, _price, _CardAddr, TransNum, _s
     console.log()
 }
 
-
-export async function mintCard(){
-    //todo
-    
+export async function MintCard(_merchantId,_seriesId, _to,_tokenURI, _price, _deadline, _signature) {
+    contract.mintCard(_merchantId,_seriesId, _to,_tokenURI, _price, _deadline, _signature);
+    console.log(_merchantId,"mint",_id,"to",_to,"custom successful!");
 }
 
-export async function getCharge(amount){
-    console.log("charge",charge);
+export async function CreateNewCardSeries(_merchantId, _seriesName, _seriesSymbol, _maxSupply){
+    contract.deployNewCardSeries(_merchantId, _seriesName, _seriesSymbol, _maxSupply)
+    console.log(_merchantId,"create",_seriesName,"success!");
+}
+
+export async function getCharge(amount) {
+    console.log("charge", charge);
     const charge = contract.getCharge(amount);
     return charge;
+}
+
+// 检查商家是否注册
+export async function checkRegisteredMerchant(_account) {
+    const bool = contract.checkIfRegisteredMerchant(_account);
+    return bool;
+}
+
+//商家注册函数
+export async function MerchantRegistration() {
+    contract.merchantRegistration();
+    console.log("merchantRegistration success!")
 }
 
 
