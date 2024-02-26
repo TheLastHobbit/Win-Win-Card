b91c53999c6306e22ffaec2c0691e8f9a4756df6
leacent <leason316@foxmail.com>
2024-02-27 02:10:19 +0800

    feat: fix top await error

--- web/src/utils/token.js
+++ web/src/utils/token.js
@@ -1,24 +1,32 @@
-import { ethers } from 'ethers';
-import ABI from '../contracts/Token.json';
+import { ethers } from "ethers";
+import ABI from "../contracts/Token.json";
 
-let provider = new ethers.BrowserProvider(window.ethereum)
+let provider = new ethers.BrowserProvider(window.ethereum);
 const contractAddress = "0x17f6eda70e4A7289e9CD57865a0DfC69313EcF58";
-const contract = new ethers.Contract(contractAddress, ABI, await provider.getSigner());
+
+const getContract = async function () {
+    const signer = await provider.getSigner();
+    return new ethers.Contract(contractAddress, ABI, signer);
+};
 
 export async function approve(spender, amount) {
+    const contract = await getContract();
     const result = await contract.approve(spender, amount);
-    console.log("tokenApproveHash:",result.hash);
-  }
+    console.log("tokenApproveHash:", result.hash);
+}
 
-export async function getTokenbalance(account){
+export async function getTokenbalance(account) {
+    const contract = await getContract();
     const balance = contract.balanceOf(account);
     return balance;
 }
 
 export async function getAllowance(owner, spender) {
-    const contract = new ethers.Contract(contractAddress, ABI, await provider.getSigner());
+    const contract = new ethers.Contract(
+        contractAddress,
+        ABI,
+        await provider.getSigner()
+    );
     const result = await contract.allowance(owner, spender);
     return Number(result);
 }
-
-
